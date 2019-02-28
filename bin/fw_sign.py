ec69db7fe7d5b4fbd80d2194e031c8aa66ba397d
Matthias L. Jugel <leo@thinkberg.com>
2019-02-28 11:56:53 +0100

    add signature verify script

--- bin/fw_sign.py
+++ bin/fw_sign.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import binascii
-import sys
 import hashlib
+import sys
 
 from ed25519 import SigningKey
 
@@ -9,8 +9,11 @@ if len(sys.argv) == 1:
     print("usage: fw_sign <firmware.bin> <privatekey.bin>")
     sys.exit(0)
 
+# open private key and read it
 with open(sys.argv[2], "rb") as privk:
     sk = SigningKey(privk.read(64))
+
+    # read firmware binary and hash it
     with open(sys.argv[1], "rb") as fw:
         sha512 = hashlib.sha512()
         while True:
@@ -20,7 +23,11 @@ with open(sys.argv[2], "rb") as privk:
 
         digest = sha512.digest()
         print("sha512   : "+bytes.decode(binascii.hexlify(digest)))
-        signed_firmware = sk.sign(sha512.digest())
-        print("signature: "+bytes.decode(binascii.hexlify(signed_firmware)))
+
+        # create signature
+        signature = sk.sign(digest)
+        print("signature: " + bytes.decode(binascii.hexlify(signature)))
+
+        # write signature to file
         with open(sys.argv[1]+".sig", "w+b") as fw_s:
-            fw_s.write(signed_firmware)
+            fw_s.write(signature)
